43f1fe8ae028381859ae6e4228be0f0031ce64d1
Gideon Asare <99325396+Yoaserr@users.noreply.github.com>
2026-01-24 13:39:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -25,4 +25,15 @@ window.addEventListener("scroll", () => {
     });
 });
 
+<script>
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+</script>
+
+
+
 
