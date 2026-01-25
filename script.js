6875f229ce544993b1401a65d2c23cc63cebe311
Gideon Asare <99325396+Yoaserr@users.noreply.github.com>
2026-01-25 11:59:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelector(".nav-links");
 
-  console.log(hamburger, navLinks); // DEBUG LINE
-
-  if (!hamburger || !navLinks) {
-    console.error("Hamburger or nav-links not found");
-    return;
-  }
+  if (!hamburger || !navLinks) return;
 
   hamburger.addEventListener("click", function () {
     navLinks.classList.toggle("show");
